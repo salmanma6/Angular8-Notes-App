79813f1895fece3121417efcc6b183619257a6a6
M A Salman <salmanma6@gmail.com>
2020-03-01 04:16:24 +0530

    Slim scroll css added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,31 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FilterPipe } from './pipes/filter-pipe';
 import { HighLightPipe } from './pipes/highlight-pipe';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 @NgModule({
   declarations: [
+
     AppComponent,
     FilterPipe,
-    HighLightPipe
+    HighLightPipe,
+   
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+     PerfectScrollbarModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
